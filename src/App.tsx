1a3e83ffa66185efe30e127ee14d1c07236e897f
Arghya <arghyadas242004@gmail.com>
2023-05-14 01:39:00 +0530

    Add: Github tate readme added

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import { useState } from 'react'
-import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+import { useState , useEffect , useRef } from 'react'
 import './App.css'
+import ReactMarkdown from 'react-markdown';
+import gfm from 'remark-gfm';
+import 'github-markdown-css/github-markdown.css';
 
 function App(): JSX.Element {
+  const textState:any = useRef() ;
   const [Mark, setMark] = useState<string>('') ;
+  useEffect(() => {
+    setMark(textState.current.value) ;
+  }, [])
+  
   return (
     <div id="App" className='hi-100'>
       <div id="nav" className='flex-row bor-r'>Markdown Editor</div>
       <div id="options" className='wi-100'>Something</div>
 
       <div id="code-block" className="flex-0 wi-100 bor-r">
-        <textarea className='code-width hi-100' onChange={(e) => setMark(e.target.value)}></textarea>
-        <ReactMarkdown className='code-width hi-100 mark-block'>{Mark}</ReactMarkdown>
+        <textarea ref={textState} placeholder='Write Here' className='code-width hi-100 code-text' onChange={(e) => setMark(e.target.value)}></textarea>
+        <ReactMarkdown
+          remarkPlugins={[gfm]} // Enable GitHub Flavored Markdown (GFM) syntax
+          className="markdown-body code-width hi-100 mark-block" // Apply GitHub Markdown CSS styles
+        >
+          {Mark}
+        </ReactMarkdown>
       </div>
 
     </div>
