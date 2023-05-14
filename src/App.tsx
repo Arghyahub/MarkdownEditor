52d82ef5589a313c3e219e87655c5a1306052fc1
Arghya <arghyadas242004@gmail.com>
2023-05-14 18:46:10 +0530

    feat: Header mode added

--- src/App.tsx
+++ src/App.tsx
@@ -29,13 +29,30 @@ function App(): JSX.Element {
     textar.value = newStr ;
     setMark(newStr) ;
   }
-  
+
+  const H1 = ():void => {
+    const textar:HTMLTextAreaElement = textState.current;
+    const start:number = textar.selectionStart ;
+    const end:number = textar.selectionEnd ;
+    const str:string = textar.value ;
+
+    if (start===end){
+      const newStr:string = str.slice(0,start) + "\n# Heading1\n" + str.slice(start) ;
+      textar.value = newStr ;
+      setMark(newStr) ;
+      return;
+    }
+
+    const newStr:string = str.slice(0,start) + "\n# " + str.slice(start,end) + "\n" + str.slice(end) ;
+    textar.value = newStr ;
+    setMark(newStr) ;
+  }  
   return (
     <div id="App" className='hi-100'>
       <div id="nav" className='flex-row bor-r'>Markdown Editor</div>
       <div id="options" className='wi-100'>
       <button className="btn" onClick={bold}>B</button>
-      <button className="btn">B</button>
+      <button className="btn" onClick={H1}>H</button>
       <button className="btn">B</button>
       <button className="btn">B</button>
       <button className="btn">B</button>
