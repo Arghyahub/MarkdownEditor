b578d3a55da80900b6b385355f5f6c3dce1300d6
Arghya <arghyadas242004@gmail.com>
2023-05-14 18:26:06 +0530

    feat : Bold text feature added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState , useEffect , useRef } from 'react'
-import './App.css'
 import ReactMarkdown from 'react-markdown';
 import gfm from 'remark-gfm';
 import 'github-markdown-css/github-markdown.css';
+import './App.css'
 
 function App(): JSX.Element {
   const textState:any = useRef() ;
@@ -10,11 +10,37 @@ function App(): JSX.Element {
   useEffect(() => {
     setMark(textState.current.value) ;
   }, [])
+
+  const bold = ():void => {
+    const textar:HTMLTextAreaElement = textState.current;
+    const start:number = textar.selectionStart ;
+    const end:number = textar.selectionEnd ;
+    const str:string = textar.value ;
+
+    if (start===end){
+      const newStr:string = str.slice(0,start) + "**BoldTextHere**" + str.slice(start) ;
+      textar.value = newStr ;
+      setMark(newStr) ;
+      return;
+    }
+
+    const newStr:string = str.slice(0,start) + "**" + str.slice(start,end) + "**" + str.slice(end) ;
+    // console.log(newStr) ;
+    textar.value = newStr ;
+    setMark(newStr) ;
+  }
   
   return (
     <div id="App" className='hi-100'>
       <div id="nav" className='flex-row bor-r'>Markdown Editor</div>
-      <div id="options" className='wi-100'>Something</div>
+      <div id="options" className='wi-100'>
+      <button className="btn" onClick={bold}>B</button>
+      <button className="btn">B</button>
+      <button className="btn">B</button>
+      <button className="btn">B</button>
+      <button className="btn">B</button>
+      <button className="btn">B</button>
+      </div>
 
       <div id="code-block" className="wi-100 bor-r">
         <textarea ref={textState} placeholder='Write Here' className='code-width hi-100 code-text' onChange={(e) => setMark(e.target.value)}></textarea>
