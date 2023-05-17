10c08b61486ad4a8be3acefc8cb70ae47c9af3ce
Arghya <arghyadas242004@gmail.com>
2023-05-17 20:24:14 +0530

    Add: moved function and made it cleaner

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import gfm from 'remark-gfm';
 import 'github-markdown-css/github-markdown.css';
 import rehypeRaw from "rehype-raw";
 
-
+import { BOLD, ITALIC , STRIKE , H1,  OL,  UL,  QUOTE , CODE , LINK , IMAGE , CHECK , TABLE } from './functions/Functions';
 import './App.css'
 
 interface refobj {
@@ -18,242 +18,23 @@ function App(): JSX.Element {
     setMark(textState.current.value) ;
   }, [])
 
-  const BOLD = ():void => {
-    const textar:HTMLTextAreaElement = textState.current;
-    const start:number = textar.selectionStart ;
-    const end:number = textar.selectionEnd ;
-    const str:string = textar.value ;
-
-    if (start===end){
-      const newStr:string = str.slice(0,start) + "**BoldTextHere** " + str.slice(start) ;
-      textar.value = newStr ;
-      setMark(newStr) ;
-      textar.selectionStart = start + 2;
-      textar.selectionEnd = start + 14 ;
-      textar.focus() ;
-      return;
-    }
-
-    const newStr:string = str.slice(0,start) + "**" + str.slice(start,end) + "**" + str.slice(end) ;
-    textar.value = newStr ;
-    setMark(newStr) ;
-  }
-
-  const ITALIC = ():void => {
-    const textar:HTMLTextAreaElement = textState.current;
-    const start:number = textar.selectionStart ;
-    const end:number = textar.selectionEnd ;
-    const str:string = textar.value ;
-
-    if (start===end){
-      const newStr:string = str.slice(0,start) + "*ITALICTextHere* " + str.slice(start) ;
-      textar.value = newStr ;
-      setMark(newStr) ;
-      textar.selectionStart = start + 1;
-      textar.selectionEnd = start + 15 ;
-      textar.focus() ;
-      return;
-    }
-
-    const newStr:string = str.slice(0,start) + "*" + str.slice(start,end) + "*" + str.slice(end) ;
-    // console.log(newStr) ;
-    textar.value = newStr ;
-    setMark(newStr) ;
-  }
-
-  const STRIKE = ():void => {
-    const textar:HTMLTextAreaElement = textState.current;
-    const start:number = textar.selectionStart ;
-    const end:number = textar.selectionEnd ;
-    const str:string = textar.value ;
-
-    if (start===end){
-      const newStr:string = str.slice(0,start) + "~~STRIKETextHere~~ " + str.slice(start) ;
-      textar.value = newStr ;
-      setMark(newStr) ;
-      textar.selectionStart = start + 2;
-      textar.selectionEnd = start + 16 ;
-      textar.focus() ;
-      return;
-    }
-
-    const newStr:string = str.slice(0,start) + "~~" + str.slice(start,end) + "~~" + str.slice(end) ;
-    // console.log(newStr) ;
-    textar.value = newStr ;
-    setMark(newStr) ;
-  }
-
-  const H1 = ():void => {
-    const textar:HTMLTextAreaElement = textState.current;
-    const start:number = textar.selectionStart ;
-    const end:number = textar.selectionEnd ;
-    const str:string = textar.value ;
-
-    if (start===end){
-      const newStr:string = str.slice(0,start) + "\n# Heading1\n" + str.slice(start) ;
-      textar.value = newStr ;
-      setMark(newStr) ;
-      
-      textar.selectionStart = start + 3;
-      textar.selectionEnd = start + 11 ;
-      textar.focus();
-      return;
-    }
-
-    const newStr:string = str.slice(0,start) + "\n# " + str.slice(start,end) + "\n" + str.slice(end) ;
-    textar.value = newStr ;
-    setMark(newStr) ;
-    textar.selectionStart = end + 3;
-    textar.selectionEnd = end + 3;
-    textar.focus();
-  }  
-
-  const OL = ():void => {
-    const textar:HTMLTextAreaElement = textState.current;
-    const start:number = textar.selectionStart ;
-    const str:string = textar.value ;
-
-    const newStr = `${str.slice(0,start)}\n\n1. List1 \n2. List2 \n3. List3 \n\n${str.slice(start)}` ;
-    textar.value = newStr ;
-    setMark(newStr) ;
-  }
-
-  const UL = ():void => {
-    const textar:HTMLTextAreaElement = textState.current;
-    const start:number = textar.selectionStart ;
-    const str:string = textar.value ;
-
-    const newStr = `${str.slice(0,start)}\n\n- List1 \n- List2 \n- List3 \n\n${str.slice(start)}` ;
-    textar.value = newStr ;
-    setMark(newStr) ;
-  }
-
-  const QUOTE = ():void => {
-    const textar:HTMLTextAreaElement = textState.current;
-    const start:number = textar.selectionStart ;
-    const end:number = textar.selectionEnd ;
-    const str:string = textar.value ;
-
-    if (start===end){
-      const newStr:string = str.slice(0,start) + "> QUOTEHere\n\n" + str.slice(start) ;
-      textar.value = newStr ;
-      setMark(newStr) ;
-      textar.selectionStart = start + 2;
-      textar.selectionEnd = start + 11 ;
-      textar.focus() ;
-      return;
-    }
-
-    const newStr:string = str.slice(0,start) + "\n> " + str.slice(start,end) + "\n\n" + str.slice(end) ;
-    textar.value = newStr ;
-    setMark(newStr) ;
-  }
-
-  const CODE = () => {
-    const textar:HTMLTextAreaElement = textState.current;
-    const start:number = textar.selectionStart ;
-    const end:number = textar.selectionEnd ;
-    const str:string = textar.value ;
-
-    if (start===end){
-      const newStr:string = str.slice(0,start) + " ```CODEhere``` " + str.slice(start) ;
-      textar.value = newStr ;
-      setMark(newStr) ;
-      textar.selectionStart = start + 4;
-      textar.selectionEnd = start + 12 ;
-      textar.focus() ;
-      return;
-    }
-
-    const newStr:string = str.slice(0,start) + "```" + str.slice(start,end) + "```" + str.slice(end) ;
-    textar.value = newStr ;
-    setMark(newStr) ;
-  }
-
-  const LINK = ():void => {
-    const textar:HTMLTextAreaElement = textState.current;
-    const start:number = textar.selectionStart ;
-    const end:number = textar.selectionEnd ;
-    const str:string = textar.value ;
-
-    if (start===end){
-      const newStr:string = str.slice(0,start) + "[TEXT](https://www.lipsum.com/) " + str.slice(end) ;
-      textar.value = newStr ;
-      setMark(newStr) ;
-      textar.selectionStart = start + 7;
-      textar.selectionEnd = start + 30 ;
-      textar.focus() ;
-      return;
-    }
-
-    const newStr:string = str.slice(0,start) + "[" + str.slice(start,end) + "](https://www.lipsum.com/)" + str.slice(end) ;
-    textar.value = newStr ;
-    setMark(newStr) ;
-    textar.selectionStart = end + 3;
-    textar.selectionEnd = end + 26 ;
-    textar.focus() ;
-  }
-
-  const IMAGE = ():void => {
-    const textar:HTMLTextAreaElement = textState.current;
-    const start:number = textar.selectionStart ;
-    const end:number = textar.selectionEnd ;
-    const str:string = textar.value ;
-
-    if (start===end){
-      const newStr:string = str.slice(0,start) + "![Title](https://loremflickr.com/640/360) " + str.slice(end) ;
-      textar.value = newStr ;
-      setMark(newStr) ;
-      textar.selectionStart = start + 9;
-      textar.selectionEnd = start + 40 ;
-      textar.focus() ;
-      return;
-    }
-
-    const newStr:string = str.slice(0,start) + "![" + str.slice(start,end) + "](https://loremflickr.com/640/360)" + str.slice(end) ;
-    textar.value = newStr ;
-    setMark(newStr) ;
-    textar.selectionStart = end + 4;
-    textar.selectionEnd = end + 35 ;
-    textar.focus() ;
-  }
-
-  const CHECK = ():void => {
-    const textar:HTMLTextAreaElement = textState.current;
-    const start:number = textar.selectionStart ;
-    const str:string = textar.value ;
-
-    const newStr = `${str.slice(0,start)}\n- [x] Task1 \n- [ ] Task2 \n- [ ] Task3 \n\n${str.slice(start)}` ;
-    textar.value = newStr ;
-    setMark(newStr) ;
-  }
-
-  const TABLE = ():void => {
-    const textar:HTMLTextAreaElement = textState.current;
-    const start:number = textar.selectionStart ;
-    const str:string = textar.value ;
-
-    const newStr = `${str.slice(0,start)}\n[comment]: <> (The distance between | and | doesn't matters)\n\n| HEAD1 | HEAD2 | HEAD3 |\n|    -  |    -  |    -  |\n| BODY  | BODY  | BODY  |\n|       |       |       |\n\n${str.slice(start)}` ;
-    textar.value = newStr ;
-    setMark(newStr) ;
-  }
 
   return (
     <div id="App" className='hi-100'>
       <div id="nav" className='flex-row bor-r'>Markdown Editor</div>
       <div id="options" className='wi-100'>
-      <button className="btn" onClick={BOLD}>B</button>
-      <button className="btn" onClick={ITALIC}>I</button>
-      <button className="btn" onClick={H1}>H</button>
-      <button className="btn" onClick={UL}>UL</button>
-      <button className="btn" onClick={OL}>OL</button>
-      <button className="btn" onClick={STRIKE}>S</button>
-      <button className="btn" onClick={QUOTE}>Q</button>
-      <button className="btn" onClick={CODE}>C</button>
-      <button className="btn" onClick={LINK}>L</button>
-      <button className="btn" onClick={IMAGE}>I</button>
-      <button className="btn" onClick={CHECK}>CH</button>
-      <button className="btn" onClick={TABLE}>T</button>
+      <button className="btn" onClick={():void => BOLD(textState.current,setMark)}>B</button>
+      <button className="btn" onClick={() => ITALIC(textState.current,setMark)}>I</button>
+      <button className="btn" onClick={() => H1(textState.current,setMark)}>H</button>
+      <button className="btn" onClick={() => UL(textState.current,setMark)}>UL</button>
+      <button className="btn" onClick={() => OL(textState.current,setMark)}>OL</button>
+      <button className="btn" onClick={() => STRIKE(textState.current,setMark)}>S</button>
+      <button className="btn" onClick={() => QUOTE(textState.current,setMark)}>Q</button>
+      <button className="btn" onClick={() => CODE(textState.current,setMark)}>C</button>
+      <button className="btn" onClick={() => LINK(textState.current,setMark)}>L</button>
+      <button className="btn" onClick={() => IMAGE(textState.current,setMark)}>I</button>
+      <button className="btn" onClick={() => CHECK(textState.current,setMark)}>CH</button>
+      <button className="btn" onClick={() => TABLE(textState.current,setMark)}>T</button>
       </div>
 
       <div className="wi-100 flex-1 coverbox">
---
+++ src/functions/Functions.ts
@@ -0,0 +1,221 @@
+const BOLD = (textar:HTMLTextAreaElement , setMark:React.Dispatch<React.SetStateAction<string>>):void => {
+  // const textar:HTMLTextAreaElement = textState.current;
+  const start:number = textar.selectionStart ;
+  const end:number = textar.selectionEnd ;
+  const str:string = textar.value ;
+
+  if (start===end){
+    const newStr:string = str.slice(0,start) + "**BoldTextHere** " + str.slice(start) ;
+    textar.value = newStr ;
+    setMark(newStr) ;
+    textar.selectionStart = start + 2;
+    textar.selectionEnd = start + 14 ;
+    textar.focus() ;
+    return;
+  }
+
+  const newStr:string = str.slice(0,start) + "**" + str.slice(start,end) + "**" + str.slice(end) ;
+  textar.value = newStr ;
+  setMark(newStr) ;
+}
+
+const ITALIC = (textar:HTMLTextAreaElement , setMark:React.Dispatch<React.SetStateAction<string>>):void => {
+  // const textar:HTMLTextAreaElement = textState.current;
+  const start:number = textar.selectionStart ;
+  const end:number = textar.selectionEnd ;
+  const str:string = textar.value ;
+
+  if (start===end){
+    const newStr:string = str.slice(0,start) + "*ITALICTextHere* " + str.slice(start) ;
+    textar.value = newStr ;
+    setMark(newStr) ;
+    textar.selectionStart = start + 1;
+    textar.selectionEnd = start + 15 ;
+    textar.focus() ;
+    return;
+  }
+
+  const newStr:string = str.slice(0,start) + "*" + str.slice(start,end) + "*" + str.slice(end) ;
+  // console.log(newStr) ;
+  textar.value = newStr ;
+  setMark(newStr) ;
+}
+
+const STRIKE = (textar:HTMLTextAreaElement , setMark:React.Dispatch<React.SetStateAction<string>>):void => {
+  // const textar:HTMLTextAreaElement = textState.current;
+  const start:number = textar.selectionStart ;
+  const end:number = textar.selectionEnd ;
+  const str:string = textar.value ;
+
+  if (start===end){
+    const newStr:string = str.slice(0,start) + "~~STRIKETextHere~~ " + str.slice(start) ;
+    textar.value = newStr ;
+    setMark(newStr) ;
+    textar.selectionStart = start + 2;
+    textar.selectionEnd = start + 16 ;
+    textar.focus() ;
+    return;
+  }
+
+  const newStr:string = str.slice(0,start) + "~~" + str.slice(start,end) + "~~" + str.slice(end) ;
+  // console.log(newStr) ;
+  textar.value = newStr ;
+  setMark(newStr) ;
+}
+
+const H1 = (textar:HTMLTextAreaElement , setMark:React.Dispatch<React.SetStateAction<string>>):void => {
+  // const textar:HTMLTextAreaElement = textState.current;
+  const start:number = textar.selectionStart ;
+  const end:number = textar.selectionEnd ;
+  const str:string = textar.value ;
+
+  if (start===end){
+    const newStr:string = str.slice(0,start) + "\n# Heading1\n" + str.slice(start) ;
+    textar.value = newStr ;
+    setMark(newStr) ;
+    
+    textar.selectionStart = start + 3;
+    textar.selectionEnd = start + 11 ;
+    textar.focus();
+    return;
+  }
+
+  const newStr:string = str.slice(0,start) + "\n# " + str.slice(start,end) + "\n" + str.slice(end) ;
+  textar.value = newStr ;
+  setMark(newStr) ;
+  textar.selectionStart = end + 3;
+  textar.selectionEnd = end + 3;
+  textar.focus();
+}  
+
+const OL = (textar:HTMLTextAreaElement , setMark:React.Dispatch<React.SetStateAction<string>>):void => {
+  // const textar:HTMLTextAreaElement = textState.current;
+  const start:number = textar.selectionStart ;
+  const str:string = textar.value ;
+
+  const newStr = `${str.slice(0,start)}\n\n1. List1 \n2. List2 \n3. List3 \n\n${str.slice(start)}` ;
+  textar.value = newStr ;
+  setMark(newStr) ;
+}
+
+const UL = (textar:HTMLTextAreaElement , setMark:React.Dispatch<React.SetStateAction<string>>):void => {
+  // const textar:HTMLTextAreaElement = textState.current;
+  const start:number = textar.selectionStart ;
+  const str:string = textar.value ;
+
+  const newStr = `${str.slice(0,start)}\n\n- List1 \n- List2 \n- List3 \n\n${str.slice(start)}` ;
+  textar.value = newStr ;
+  setMark(newStr) ;
+}
+
+const QUOTE = (textar:HTMLTextAreaElement , setMark:React.Dispatch<React.SetStateAction<string>>):void => {
+  // const textar:HTMLTextAreaElement = textState.current;
+  const start:number = textar.selectionStart ;
+  const end:number = textar.selectionEnd ;
+  const str:string = textar.value ;
+
+  if (start===end){
+    const newStr:string = str.slice(0,start) + "> QUOTEHere\n\n" + str.slice(start) ;
+    textar.value = newStr ;
+    setMark(newStr) ;
+    textar.selectionStart = start + 2;
+    textar.selectionEnd = start + 11 ;
+    textar.focus() ;
+    return;
+  }
+
+  const newStr:string = str.slice(0,start) + "\n> " + str.slice(start,end) + "\n\n" + str.slice(end) ;
+  textar.value = newStr ;
+  setMark(newStr) ;
+}
+
+const CODE = (textar:HTMLTextAreaElement , setMark:React.Dispatch<React.SetStateAction<string>>) => {
+  // const textar:HTMLTextAreaElement = textState.current;
+  const start:number = textar.selectionStart ;
+  const end:number = textar.selectionEnd ;
+  const str:string = textar.value ;
+
+  if (start===end){
+    const newStr:string = str.slice(0,start) + " ```CODEhere``` " + str.slice(start) ;
+    textar.value = newStr ;
+    setMark(newStr) ;
+    textar.selectionStart = start + 4;
+    textar.selectionEnd = start + 12 ;
+    textar.focus() ;
+    return;
+  }
+
+  const newStr:string = str.slice(0,start) + "```" + str.slice(start,end) + "```" + str.slice(end) ;
+  textar.value = newStr ;
+  setMark(newStr) ;
+}
+
+const LINK = (textar:HTMLTextAreaElement , setMark:React.Dispatch<React.SetStateAction<string>>):void => {
+  // const textar:HTMLTextAreaElement = textState.current;
+  const start:number = textar.selectionStart ;
+  const end:number = textar.selectionEnd ;
+  const str:string = textar.value ;
+
+  if (start===end){
+    const newStr:string = str.slice(0,start) + "[TEXT](https://www.lipsum.com/) " + str.slice(end) ;
+    textar.value = newStr ;
+    setMark(newStr) ;
+    textar.selectionStart = start + 7;
+    textar.selectionEnd = start + 30 ;
+    textar.focus() ;
+    return;
+  }
+
+  const newStr:string = str.slice(0,start) + "[" + str.slice(start,end) + "](https://www.lipsum.com/)" + str.slice(end) ;
+  textar.value = newStr ;
+  setMark(newStr) ;
+  textar.selectionStart = end + 3;
+  textar.selectionEnd = end + 26 ;
+  textar.focus() ;
+}
+
+const IMAGE = (textar:HTMLTextAreaElement , setMark:React.Dispatch<React.SetStateAction<string>>):void => {
+  // const textar:HTMLTextAreaElement = textState.current;
+  const start:number = textar.selectionStart ;
+  const end:number = textar.selectionEnd ;
+  const str:string = textar.value ;
+
+  if (start===end){
+    const newStr:string = str.slice(0,start) + "![Title](https://loremflickr.com/640/360) " + str.slice(end) ;
+    textar.value = newStr ;
+    setMark(newStr) ;
+    textar.selectionStart = start + 9;
+    textar.selectionEnd = start + 40 ;
+    textar.focus() ;
+    return;
+  }
+
+  const newStr:string = str.slice(0,start) + "![" + str.slice(start,end) + "](https://loremflickr.com/640/360)" + str.slice(end) ;
+  textar.value = newStr ;
+  setMark(newStr) ;
+  textar.selectionStart = end + 4;
+  textar.selectionEnd = end + 35 ;
+  textar.focus() ;
+}
+
+const CHECK = (textar:HTMLTextAreaElement , setMark:React.Dispatch<React.SetStateAction<string>>):void => {
+  // const textar:HTMLTextAreaElement = textState.current;
+  const start:number = textar.selectionStart ;
+  const str:string = textar.value ;
+
+  const newStr = `${str.slice(0,start)}\n- [x] Task1 \n- [ ] Task2 \n- [ ] Task3 \n\n${str.slice(start)}` ;
+  textar.value = newStr ;
+  setMark(newStr) ;
+}
+
+const TABLE = (textar:HTMLTextAreaElement , setMark:React.Dispatch<React.SetStateAction<string>>):void => {
+  // const textar:HTMLTextAreaElement = textState.current;
+  const start:number = textar.selectionStart ;
+  const str:string = textar.value ;
+
+  const newStr = `${str.slice(0,start)}\n[comment]: <> (The distance between | and | doesn't matters)\n\n| HEAD1 | HEAD2 | HEAD3 |\n|    -  |    -  |    -  |\n| BODY  | BODY  | BODY  |\n|       |       |       |\n\n${str.slice(start)}` ;
+  textar.value = newStr ;
+  setMark(newStr) ;
+}
+
+export { BOLD, ITALIC , STRIKE , H1,  OL,  UL,  QUOTE , CODE , LINK , IMAGE , CHECK , TABLE };
