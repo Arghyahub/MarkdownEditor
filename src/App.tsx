c2d274e8169b62f067b2b80111e7483d4ebc71ef
Arghya <arghyadas242004@gmail.com>
2023-05-20 02:55:12 +0530

    add: design change

--- src/App.tsx
+++ src/App.tsx
@@ -23,18 +23,18 @@ function App(): JSX.Element {
     <div id="App" className='hi-100'>
       <div id="nav" className='flex-row bor-r'>Markdown Editor</div>
       <div id="options" className='wi-100'>
-      <button className="btn" onClick={():void => BOLD(textState.current,setMark)}>B</button>
-      <button className="btn" onClick={() => ITALIC(textState.current,setMark)}><i>I</i></button>
-      <button className="btn" onClick={() => H1(textState.current,setMark)}><img className='btn-img' src="./heading.svg" alt="H1" /></button>
-      <button className="btn" onClick={() => UL(textState.current,setMark)}><img className='btn-img' src="./ulist.svg" alt="UL" /></button>
-      <button className="btn" onClick={() => OL(textState.current,setMark)}><img className='btn-img' src="./olist.svg" alt="OL" /></button>
-      <button className="btn" onClick={() => STRIKE(textState.current,setMark)}><s>S</s></button>
-      <button className="btn" onClick={() => QUOTE(textState.current,setMark)}><img className='btn-img' src="./quote.svg" alt="Q" /></button>
-      <button className="btn" onClick={() => CODE(textState.current,setMark)}>{"</>"}</button>
-      <button className="btn" onClick={() => LINK(textState.current,setMark)}><img className='btn-img' src="./link.svg" alt="L" /></button>
-      <button className="btn" onClick={() => IMAGE(textState.current,setMark)}><img className='btn-img' src="./image.svg" alt="I" /></button>
-      <button className="btn" onClick={() => CHECK(textState.current,setMark)}><img className='btn-img' src="./check.svg" alt="CH" /></button>
-      <button className="btn" onClick={() => TABLE(textState.current,setMark)}><img className='btn-img' src="./table.svg" alt="T" /></button>
+      <button className="btn" onClick={():void => BOLD(textState.current,setMark)}>B <div className="what">Bold</div></button>
+      <button className="btn" onClick={() => ITALIC(textState.current,setMark)}><i>I</i> <div className="what">Italic</div></button>
+      <button className="btn" onClick={() => H1(textState.current,setMark)}><img className='btn-img' src="./heading.svg" alt="H1" /> <div className="what">Header</div></button>
+      <button className="btn" onClick={() => UL(textState.current,setMark)}><img className='btn-img' src="./ulist.svg" alt="UL" /> <div className="what">Unordered List</div></button>
+      <button className="btn" onClick={() => OL(textState.current,setMark)}><img className='btn-img' src="./olist.svg" alt="OL" /> <div className="what">Ordered List</div></button>
+      <button className="btn" onClick={() => STRIKE(textState.current,setMark)}><s>S</s> <div className="what">Strike</div></button>
+      <button className="btn" onClick={() => QUOTE(textState.current,setMark)}><img className='btn-img' src="./quote.svg" alt="Q" /> <div className="what">Quote</div></button>
+      <button className="btn" onClick={() => CODE(textState.current,setMark)}>{"</>"} <div className="what">CodeBlock</div></button>
+      <button className="btn" onClick={() => LINK(textState.current,setMark)}><img className='btn-img' src="./link.svg" alt="L" /> <div className="what">Links</div></button>
+      <button className="btn" onClick={() => IMAGE(textState.current,setMark)}><img className='btn-img' src="./image.svg" alt="I" /> <div className="what">Image</div></button>
+      <button className="btn" onClick={() => CHECK(textState.current,setMark)}><img className='btn-img' src="./check.svg" alt="CH" /> <div className="what">Checkbox</div></button>
+      <button className="btn" onClick={() => TABLE(textState.current,setMark)}><img className='btn-img' src="./table.svg" alt="T" /> <div className="what">Table</div></button>
       </div>
 
       <div className="wi-100 flex-1 coverbox">
